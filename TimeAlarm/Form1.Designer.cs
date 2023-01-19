5f25f210565a4e9d500858cf2a0ce051e3de52ad
ys <ys@prov.com.tw>
2023-01-19 10:18:16 +0800

    First upload

---
+++ TimeAlarm/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TimeAlarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.tTime = new System.Windows.Forms.Timer(this.components);
+            this.tbTimeToAlarm = new System.Windows.Forms.TextBox();
+            this.btnTimeToAlarm = new System.Windows.Forms.Button();
+            this.btnTimeEndAlarm = new System.Windows.Forms.Button();
+            this.tbTimeEndAlarm = new System.Windows.Forms.TextBox();
+            this.lbTipTimeToAlarm = new System.Windows.Forms.Label();
+            this.lbTimeRemain = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbTime.Location = new System.Drawing.Point(54, 24);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(51, 19);
+            this.lbTime.TabIndex = 0;
+            this.lbTime.Text = "Time";
+            // 
+            // tTime
+            // 
+            this.tTime.Enabled = true;
+            this.tTime.Interval = 500;
+            this.tTime.Tick += new System.EventHandler(this.tTime_Tick);
+            // 
+            // tbTimeToAlarm
+            // 
+            this.tbTimeToAlarm.Location = new System.Drawing.Point(12, 67);
+            this.tbTimeToAlarm.Name = "tbTimeToAlarm";
+            this.tbTimeToAlarm.Size = new System.Drawing.Size(131, 22);
+            this.tbTimeToAlarm.TabIndex = 1;
+            this.tbTimeToAlarm.Click += new System.EventHandler(this.tbTimeToAlarm_Click);
+            // 
+            // btnTimeToAlarm
+            // 
+            this.btnTimeToAlarm.Location = new System.Drawing.Point(175, 65);
+            this.btnTimeToAlarm.Name = "btnTimeToAlarm";
+            this.btnTimeToAlarm.Size = new System.Drawing.Size(57, 23);
+            this.btnTimeToAlarm.TabIndex = 2;
+            this.btnTimeToAlarm.Text = "Set";
+            this.btnTimeToAlarm.UseVisualStyleBackColor = true;
+            this.btnTimeToAlarm.Click += new System.EventHandler(this.btnTimeToAlarm_Click);
+            // 
+            // btnTimeEndAlarm
+            // 
+            this.btnTimeEndAlarm.Location = new System.Drawing.Point(175, 104);
+            this.btnTimeEndAlarm.Name = "btnTimeEndAlarm";
+            this.btnTimeEndAlarm.Size = new System.Drawing.Size(57, 23);
+            this.btnTimeEndAlarm.TabIndex = 2;
+            this.btnTimeEndAlarm.Text = "Set";
+            this.btnTimeEndAlarm.UseVisualStyleBackColor = true;
+            this.btnTimeEndAlarm.Click += new System.EventHandler(this.btnTimeEndAlarm_Click);
+            // 
+            // tbTimeEndAlarm
+            // 
+            this.tbTimeEndAlarm.Location = new System.Drawing.Point(12, 106);
+            this.tbTimeEndAlarm.Name = "tbTimeEndAlarm";
+            this.tbTimeEndAlarm.Size = new System.Drawing.Size(131, 22);
+            this.tbTimeEndAlarm.TabIndex = 1;
+            this.tbTimeEndAlarm.Text = "00h03m00s";
+            this.tbTimeEndAlarm.Click += new System.EventHandler(this.tbTimeEndAlarm_Click);
+            // 
+            // lbTipTimeToAlarm
+            // 
+            this.lbTipTimeToAlarm.AutoSize = true;
+            this.lbTipTimeToAlarm.Location = new System.Drawing.Point(12, 140);
+            this.lbTipTimeToAlarm.Name = "lbTipTimeToAlarm";
+            this.lbTipTimeToAlarm.Size = new System.Drawing.Size(87, 12);
+            this.lbTipTimeToAlarm.TabIndex = 3;
+            this.lbTipTimeToAlarm.Text = "TipTimeToAlarm";
+            // 
+            // lbTimeRemain
+            // 
+            this.lbTimeRemain.AutoSize = true;
+            this.lbTimeRemain.Font = new System.Drawing.Font("新細明體", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbTimeRemain.Location = new System.Drawing.Point(12, 184);
+            this.lbTimeRemain.Name = "lbTimeRemain";
+            this.lbTimeRemain.Size = new System.Drawing.Size(188, 35);
+            this.lbTimeRemain.TabIndex = 4;
+            this.lbTimeRemain.Text = "TimeRemain";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbTimeRemain);
+            this.Controls.Add(this.lbTipTimeToAlarm);
+            this.Controls.Add(this.btnTimeEndAlarm);
+            this.Controls.Add(this.btnTimeToAlarm);
+            this.Controls.Add(this.tbTimeEndAlarm);
+            this.Controls.Add(this.tbTimeToAlarm);
+            this.Controls.Add(this.lbTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Timer tTime;
+        private System.Windows.Forms.TextBox tbTimeToAlarm;
+        private System.Windows.Forms.Button btnTimeToAlarm;
+        private System.Windows.Forms.Button btnTimeEndAlarm;
+        private System.Windows.Forms.TextBox tbTimeEndAlarm;
+        private System.Windows.Forms.Label lbTipTimeToAlarm;
+        private System.Windows.Forms.Label lbTimeRemain;
+    }
+}
+
---
+++ TimeAlarm/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeAlarm
+{
+    public partial class Form1 : Form
+    {
+        bool bTimeToAlarm = false;
+        bool bChangeSet = false;
+        DateTime tSetTimeToAlarm;
+
+        bool bTimeEndAlarm = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tTime_Tick(object sender, EventArgs e)
+        {
+            lbTime.Text = DateTime.Now.ToString("yyyy'-'MM'-'dd' 'HH':'mm':'ss");
+
+            if (bTimeToAlarm || bTimeEndAlarm)
+            {
+                if (DateTime.Compare(tSetTimeToAlarm, DateTime.Now) <= 0)
+                {
+                    lbTimeRemain.ForeColor = Color.Black;
+                    tTime.Stop();
+                    bTimeToAlarm = false;
+                    DialogResult dReturn = MessageBox.Show("按確定，再次重複倒數計時", "Time Alarm", MessageBoxButtons.OKCancel,
+                                                            MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    if (dReturn == System.Windows.Forms.DialogResult.OK)
+                    {
+                        if (bTimeEndAlarm)
+                        {
+                            char[] separators = new char[] { 'h', 'm', 's' };
+                            string[] subs = tbTimeEndAlarm.Text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                            int iTotalSec = Convert.ToInt32(subs[0]) * 60 * 60;
+                            iTotalSec += Convert.ToInt32(subs[1]) * 60;
+                            iTotalSec += Convert.ToInt32(subs[2]);
+                            tSetTimeToAlarm = DateTime.Now.AddSeconds(iTotalSec);
+
+                            lbTipTimeToAlarm.Text = tSetTimeToAlarm.ToString("yyyy'-'MM'-'dd' 'HH':'mm':'ss");
+                            bTimeEndAlarm = true;
+                            bChangeSet = false;
+
+                            tTime.Start();
+                        }
+                        else
+                        {
+                            tbTimeEndAlarm.Text = "00h03m00s";
+                            tTime.Stop();
+                            this.Close();
+                        }
+
+                    }
+                    else
+                    {
+                        tbTimeEndAlarm.Text = "00h03m00s";
+                        tTime.Stop();
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    lbTimeRemain.ForeColor = Color.Green;
+                    TimeSpan span = tSetTimeToAlarm.Subtract(DateTime.Now);
+                    lbTimeRemain.Text = ((span.Days > 0) ? span.Days + " D " : "") + span.Hours + " H " + span.Minutes + " M " + span.Seconds + " S";
+                }
+            }
+        }
+
+        private void tbTimeToAlarm_Click(object sender, EventArgs e)
+        {
+            if (bChangeSet == false)
+            {
+                bChangeSet = true;
+                tbTimeToAlarm.Text = lbTime.Text;
+            }
+        }
+
+        private void tbTimeEndAlarm_Click(object sender, EventArgs e)
+        {
+            if (bChangeSet == false)
+            {
+                bChangeSet = true;
+                tbTimeEndAlarm.Text = "00h03m00s";
+            }
+        }
+
+        private void btnTimeToAlarm_Click(object sender, EventArgs e)
+        {
+            if (tbTimeToAlarm.Text.Length <= 0)
+                return;
+
+            bChangeSet = false;
+            DateTime tAlarm = DateTime.Parse(tbTimeToAlarm.Text);
+
+            /*
+            小於零	t1 早於 t2。
+            零	t1 與 t2 相同。
+            大於零	t1 晚於 t2。
+             */
+            if (DateTime.Compare(tAlarm, DateTime.Now) > 0)
+            {
+                tSetTimeToAlarm = tAlarm;
+                bTimeToAlarm = true;
+                tTime.Start();
+            }
+            else 
+            {
+                tbTimeToAlarm.Text = lbTime.Text;
+            }
+        }
+
+        private void btnTimeEndAlarm_Click(object sender, EventArgs e)
+        {
+            if (!tbTimeEndAlarm.Text.Contains('h') || !tbTimeEndAlarm.Text.Contains('m') || !tbTimeEndAlarm.Text.Contains('s'))
+            {
+                tbTimeEndAlarm.Text = "00h03m00s";
+                return;
+            }
+
+            char[] separators = new char[] { 'h', 'm', 's' };
+            string[] subs = tbTimeEndAlarm.Text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            if (subs.Length != 3)
+            {
+                tbTimeEndAlarm.Text = "00h03m00s";
+                return;
+            }
+
+            int iTotalSec = Convert.ToInt32(subs[0]) * 60 * 60;
+            iTotalSec += Convert.ToInt32(subs[1]) * 60;
+            iTotalSec += Convert.ToInt32(subs[2]);
+            tSetTimeToAlarm = DateTime.Now.AddSeconds(iTotalSec);
+
+            lbTipTimeToAlarm.Text = tSetTimeToAlarm.ToString("yyyy'-'MM'-'dd' 'HH':'mm':'ss");
+            bTimeEndAlarm = true;
+            bChangeSet = false;
+        }
+    }
+}
